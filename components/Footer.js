e8e5fa64a4e201ecf0d5581926241b4fd267bee3
izrael707 <dkingisreal@gmail.com>
2023-03-14 18:02:47 +0100

    launch

---
+++ components/Footer.js
@@ -0,0 +1,5 @@
+const Footer = () => {
+  return <footer>copyrights 2023 Ninja List</footer>;
+};
+
+export default Footer;
---
+++ pages/about.js
@@ -0,0 +1,23 @@
+import Head from "next/head";
+
+const About = () => {
+  return (
+    <>
+      <Head>
+        <meta name="keywords" content="ninjas" />
+        <title>Ninja List | About</title>
+      </Head>
+      <div>
+        <h1>About Page</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae,
+          consequuntur rem dolorum pariatur vitae magnam officia provident
+          soluta rerum voluptatum libero incidunt amet numquam minima facilis
+          excepturi ut ea molestias.
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default About;
